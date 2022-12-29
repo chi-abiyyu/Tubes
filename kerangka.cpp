962c030c6166d99bb10e5907464c405668530bb3
Chiel_Abiyyu <67826596+chi-abiyyu@users.noreply.github.com>
2022-12-29 21:08:16 +0700

    revisi 1 kerangka.cpp

--- kerangka.cpp
+++ kerangka.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-float harga(int kuant, int harga){
-    return kuant * harga;
+long harga(int kuant, int harga, int inap){
+    return kuant * harga * inap;
+}
+
+string tiket (string nama, string cek_in, string cek_out){
+    cout << "--------------TIKER HOTEL BLABLABLA--------------\n";
+    cout << "Nama Pemesan ; " << nama << endl;
+    cout << "Tanggal Cek In : " << cek_in << endl;
+    cout << "Tanggal Cek Out : " << cek_out << endl;
+    cout << "__________________________________________________\n";
+    cout << "               Terimakasih ";
 }
 int main(){
     int pilih_1;
@@ -14,11 +25,9 @@ int main(){
     cout << "3" << endl;
     cout << "Masukan Pilihan 1-3 : " ;
     cin >> pilihan;
-    switch (pilihan)
-    {
-    case 1 :
-    cout << "1" << endl;
-    loop_1 :
+    if (pilihan == 1){
+        cout << "1" << endl;
+        loop_1 :
         cout << "Menu" << endl;
         cout << "1. fas" << endl;
         cout << "2. fas" << endl;
@@ -50,37 +59,61 @@ int main(){
         else{
             goto loop_1;
         }
-        break;
-    case 2 :
+    }
+    else if(pilihan == 2){
+
+    //Formulir
+    //Identitas, Tanggal Chek Out n In, Jenis Kamar, Layanan Tambahan, Pembayaran
+    string nama;
+    string cek_in;
+    string cek_out;
+    int inap;
+    
+    cout << "Masukan Nama : ";
+    getline(cin, nama);
+
+    cout << "Lama Inap : ";
+    cin >> inap;
+    
+    cin.ignore();
+    
+    cout << "Tanggak Cek In (dd/mm/yy): ";
+    getline(cin, cek_in);
+
+    cout << "Tangggal Cek Out (dd/mm/yy) : ";
+    getline(cin, cek_out);
+
     int pilih_harga;
     int jmlh_kamar;
+    long standar = 200000;
+    long deluxe = 500000;
     loop_room_3 :
     cout << "Menu Harga :\n";
-    cout << "1. Standar : 200000\n";
-    cout << "2. Deluxe : 500000\n";
+    cout << "1. Standar\n";
+    cout << "2. Deluxe\n";
     cout << "Masukan pilihan :";
     cin >> pilih_harga;
-    cout << "jumlah kamar : ";
-    cin >> jmlh_kamar;
-    if (1){
-        cout << "Harga :" << harga(jmlh_kamar, 200000) << endl;
+    if (pilih_harga == 1){
+        cout << "jumlah kamar : ";
+        cin >> jmlh_kamar;
+        cout << "Harga : Rp." << harga(jmlh_kamar, standar, inap) << endl;
+        tiket(nama, cek_in, cek_out);
     }
-    else if (1){
-        cout << "Harga :" << harga(jmlh_kamar, 500000) << endl;
+    else if (pilih_harga == 2){
+        cout << "jumlah kamar : ";
+        cin >> jmlh_kamar;
+        cout << "Harga :" << harga(jmlh_kamar, deluxe, inap) << endl;
+        tiket(nama, cek_in, cek_out);
     }
     else{
         cout << "anda salah input\n";
         goto loop_room_3;
     }
-    break;
-    case 3 :
-    string layanan_tambahan;
-    cout << "Masukan Tambahan Layanan :" << endl;
-    
-    default:
-    cout << "Maaf anda salah input"<< endl;
-    goto loop_menu;
-        break;
+    }
+    else{
+        cout << "     Maaf Anda Salah Input   \n ";
+        cout << "Harap Isi Ulang Dengan Benar \n";
+        goto loop_menu;
     }
 
 }
